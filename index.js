be0341d6bcd9c1a0cfbc27f45fc4e328ee532452
Alexsander <alexsander11115@gmail.com>
2019-05-21 11:52:23 +0300

    Add header,lib

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 let express = require('express');
-
+const path = require('path');
 let mysql = require('mysql');
 
+let app = express();
+
 const DB_PREFIX = 'oc_';
+const port = 3000;
+const pathName = '127.0.0.1';
 
 let con = mysql.createConnection({
     host: "127.0.0.1",
@@ -16,37 +20,28 @@ app.set('views',path.join(__dirname,'views'));
 app.set('view engine','pug');
 
 app.use(express.static('public'));
-app.use('/js', express.static('../node_modules/jquery/dist'));
-app.use('/js', express.static('../node_modules/bootstrap/dist/js'));
-app.use('/css', express.static('../node_modules/bootstrap/dist/css'));
-
-
-
-
-let app = express();
-
-
-
-const port = 3000;
-const pathName = '127.0.0.1';
-
-
 
 app.get('/',function (req,res) {
     // console.log(req);
-    con.connect(function(err) {
-        if (err) throw err;
-
-        con.query("SELECT * FROM oc_customer WHERE customer_id = 1", function (err, result, fields) {
-            if (err) throw err;
-
-            res.render('main',{
-                results: JSON.parse(JSON.stringify(result))
-            });
-
-        });
-
+    let sql = `SELECT * FROM  ${DB_PREFIX}category c LEFT JOIN ${DB_PREFIX}category_description cd ON (c.category_id = cd.category_id) LEFT JOIN ${DB_PREFIX}category_to_store c2s ON (c.category_id = c2s.category_id)  WHERE c.parent_id = 0 AND c.status = '1' ORDER BY c.sort_order, LCASE(cd.name)`;
+    // console.log(sql);
+    let categories  = new Promise(function (resolve, reject) {
+        con.query(
+            sql,
+            function (error,result) {
+                if(error) reject(err);
+                resolve(result)
+            }
+        )
     });
+
+
+    Promise.all([categories]).then(function (value) {
+        // console.log(JSON.parse(JSON.stringify(value[0].length)));
+        res.render('main',{
+            categories: JSON.parse(JSON.stringify(value[0])),
+        })
+    })
 
 });
 
@@ -99,7 +94,7 @@ app.get('/categories',function (req,res) {
 
 
     Promise.all([categories]).then(function (value) {
-        // console.log(JSON.parse(JSON.stringify(value[0])));
+        // console.log(JSON.parse(JSON.stringify(value[0].length)));
         res.render('categories',{
             categories: JSON.parse(JSON.stringify(value[0])),
         })
