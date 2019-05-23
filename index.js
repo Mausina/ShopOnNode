881a80214189b5a6ff666d696bed23758dcff470
Alexsander <alexsander11115@gmail.com>
2019-05-23 14:15:33 +0300

    Add some include,css and footer

--- index.js
+++ index.js
@@ -22,9 +22,11 @@ app.set('view engine','pug');
 app.use(express.static('public'));
 
 app.get('/',function (req,res) {
-    // console.log(req);
-    let sql = `SELECT * FROM  ${DB_PREFIX}category c LEFT JOIN ${DB_PREFIX}category_description cd ON (c.category_id = cd.category_id) LEFT JOIN ${DB_PREFIX}category_to_store c2s ON (c.category_id = c2s.category_id)  WHERE c.parent_id = 0 AND c.status = '1' ORDER BY c.sort_order, LCASE(cd.name)`;
-    // console.log(sql);
+
+    let sql = `SELECT * FROM  ${DB_PREFIX}category c LEFT JOIN ${DB_PREFIX}category_description cd ON (c.category_id = cd.category_id) WHERE parent_id = 0`;
+    let sqlBanner = `SELECT image FROM ${DB_PREFIX}banner_image`;
+    let sqlFeatured = `SELECT op.product_id,model,image,name FROM ${DB_PREFIX}product AS op JOIN oc_product_description AS opd WHERE op.product_id = opd.product_id LIMIT 0,3`;
+
     let categories  = new Promise(function (resolve, reject) {
         con.query(
             sql,
@@ -34,12 +36,31 @@ app.get('/',function (req,res) {
             }
         )
     });
+    let banners  = new Promise(function (resolve, reject) {
+        con.query(
+            sqlBanner,
+            function (error,result) {
+                if(error) reject(err);
+                resolve(result)
+            }
+        )
+    });
+    let featured = new Promise(function (resolve, reject) {
+        con.query(
+            sqlFeatured,
+            function (error,result) {
+                if(error) reject(err);
+                resolve(result)
+            }
+        )
+    });
 
-
-    Promise.all([categories]).then(function (value) {
-        // console.log(JSON.parse(JSON.stringify(value[0].length)));
+    Promise.all([categories,banners,featured]).then(function (value) {
+        // console.log(JSON.parse(JSON.stringify(value[2])));
         res.render('main',{
             categories: JSON.parse(JSON.stringify(value[0])),
+            banners: JSON.parse(JSON.stringify(value[1])),
+            featured: JSON.parse(JSON.stringify(value[2])),
         })
     })
 
--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-    $(".owl-carousel").owlCarousel();
+    $('.owl-carousel').owlCarousel({
+        loop:true,
+        margin:10,
+        // autoWidth:true,
+        autoHeight:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1,
+                nav:false
+            },
+            600:{
+                items:1,
+                nav:false
+            },
+            1000:{
+                items:1,
+                nav:false,
+                loop:false
+            }
+        }
+    })
 });
